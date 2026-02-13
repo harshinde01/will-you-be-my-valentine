3c277c9a008fad5d14948b009f8415eb5c6a5d48
Harsh Shinde <harsh.sh8958@gmail.com>
2026-02-13 00:15:11 -0500

    Refactor no button phrases and update images/text

--- src/App.tsx
+++ src/App.tsx
@@ -13,21 +13,19 @@ export default function Page() {
   const getNoButtonText = () => {
     const phrases = [
       "No",
-      "Are you sure?",
-      "What if I asked really nicely?",
-      "Pretty please",
-      "With a chocolate rice cake on top",
-      "What about a matcha frostie",
-      "PLEASE POOKIE",
-      "But :*(",
-      "I am going to die",
-      "Yep im dead",
-      "ok ur talking to nathan's ghost",
-      "please babe",
-      ":((((",
-      "PRETTY PLEASE",
-      "Estoy muerto",
-      "No :(",
+      "are u sure?",
+      "what if I asked really nicely?",
+      "pretty please",
+      "with a cherry on top",
+      "well you don't like cherries",
+      "with a strawberry on top?",
+      "But :(",
+      "why not",
+      "stop",
+      "just say yes",
+      "no :(",
+      "last chance",
+      "fine I don't want to be your valentine",
     ];
 
     return phrases[Math.min(noCount, phrases.length - 1)];
@@ -37,16 +35,16 @@ export default function Page() {
     <div className="-mt-16 flex h-screen flex-col items-center justify-center">
       {yesPressed ? (
         <>
-          <img src="https://media.tenor.com/gUiu1zyxfzYAAAAi/bear-kiss-bear-kisses.gif" />
-          <div className="my-4 text-4xl font-bold">WOOOOOO!!! I love you pookie!! ;))</div>
+          <img src="https://media.tenor.com/4bV9ylEOWpgAAAAj/bubu-dudu-sseeyall.gif" />
+          <div className="my-4 text-4xl font-bold">yay mwah :)</div>
         </>
       ) : (
         <>
           <img
             className="h-[200px]"
-            src="https://gifdb.com/images/high/cute-love-bear-roses-ou7zho5oosxnpo6k.gif"
+            src="https://media.gifdb.com/dudu-kissing-bubu-hearts-gif-ohi53sgm3xj46ra2.gif"
           />
-          <h1 className="my-4 text-4xl">Will you be my Valentine?</h1>
+          <h1 className="my-4 text-4xl">Hi baby:) Will you be my Valentine?</h1>
           <div className="flex items-center">
             <button
               className={`mr-4 rounded bg-green-500 px-4 py-2 font-bold text-white hover:bg-green-700`}
